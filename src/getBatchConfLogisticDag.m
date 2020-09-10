6fc4eb903aa78beaab3c099b437cc5c04fd6489b
Vojta <vojtech.franc@gmail.com>
2020-09-10 15:37:49 +0200

    bug

--- src/getBatchConfLogisticDag.m
+++ src/getBatchConfLogisticDag.m
@@ -1,12 +1,20 @@
-function inputs = getBatchLogisticDag(Opts,ImDb, batch)
+function inputs = getBatchLogisticDag(Opts,ImDb, batch, onlyInput)
 %% 
-
+     if nargin < 4
+            onlyInput = 0;
+     end
+    
     images = single( ImDb.images.data(:,:,:,batch) );
 
     if ~isempty(Opts.gpus) > 0
         images = gpuArray(images) ;
     end
-        
+    
+    if onlyInput
+        inputs = { 'input', images } ;
+        return;
+    end
+
     if isfield( ImDb.images, 'loss') 
         loss = ImDb.images.loss(batch) ;
         predY = ImDb.images.predY(batch) ;
