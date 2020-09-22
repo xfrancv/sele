a0e37a7954145a36cbe26788434a59ab30a015ac
Vojta <vojtech.franc@gmail.com>
2020-09-22 14:56:28 +0200

    updates

--- ordreg/run_all_train_conf_trndata.m
+++ ordreg/run_all_train_conf_trndata.m
@@ -1,12 +1,26 @@
 run('../selclassif_setpath.m');
 
-dataSet = {{'california1', [100 500 1000 5000 6000],1000,1000},...
-           {'abalone1', [100 500 1000 1200], 100,100},...
-           {'bank1',[100 500 1000 1254],500,1000 },...
-           {'cpu1',[100 500 1000 2000 2459],2000,1000 }};
+dataSet = {{'california1', [100 500 1000 5000 6000],1000,1000,50},...
+           {'abalone1', [100 500 1000 1200], 100,100,50},...
+           {'bank1',[100 500 1000 1254],500,1000,100 },...
+           {'cpu1',[100 500 1000 2000 2459],2000,1000,50 }};
 
     
-           
+
+%% 
+for i = 1 : numel( dataSet )
+    for trnData = dataSet{i}{2}
+        run_train_conf_hinge_mlp( dataSet{i}{1}, 'zmuv', dataSet{i}{5}, trnData);    
+    end
+end
+
+%%
+for i = 1 : numel( dataSet )
+    for trnData = dataSet{i}{2}
+        run_train_conf_regression_mlp( dataSet{i}{1}, 'zmuv', trnData);    
+    end
+end
+       
 %% 
 for i = 1 : numel( dataSet )
     for trnData = dataSet{i}{2}
