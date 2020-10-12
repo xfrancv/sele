85f26d80ebada8467e20f2597980dfb38250dafc
Vojta <vojtech.franc@gmail.com>
2020-10-12 20:45:33 +0200

    new dataset

--- ordreg/run_all_train_classif.m
+++ ordreg/run_all_train_classif.m
@@ -1,7 +1,7 @@
 run('../selclassif_setpath.m');
 
-dataSet = {'california1', 'abalone1','bank1','cpu1','msd1'};
-       
+dataSet = {'california1', 'abalone1','bank1','cpu1','msd1','facebook1'};
+
 
 for i = 1 : numel( dataSet )
     run_train_svorimc( dataSet{i}, 'zmuv' );
